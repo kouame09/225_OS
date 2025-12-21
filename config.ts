044265f36f85284118a5675d789d36b53a3a1220
kouame09 <princekouame7@gmail.com>
2025-12-21 16:43:21 +0000

    Close

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const CONFIG = {
     // Toggle maintenance mode
-    IS_MAINTENANCE_MODE: false, // Set to false to disable maintenance mode
+    IS_MAINTENANCE_MODE: true, // Set to false to disable maintenance mode
 };
